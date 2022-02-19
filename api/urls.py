9e08bd6249a851798cce804ae1f601a9399283ef
SalihTpc <salihtopcu@hotmail.com.tr>
2022-02-19 18:05:31 +0300

    create update and delete func. added

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import apiOverView, taskDetail, taskList
+from .views import apiOverView, taskDelete, taskDetail, taskList, taskCreate, taskUpdate
 
 urlpatterns = [
     path("", apiOverView, name="api-overview"),
     path("task-list/", taskList, name="task-list"),
-    path("task-detail/<str:pk>", taskDetail, name="task-list")
+    path("task-detail/<str:pk>", taskDetail, name="task-list"),
+    path('task-create/', taskCreate, name="task-create"),
+    path("task-update/<str:pk>", taskUpdate, name="task-update"),
+    path("task-delete/<str:pk>", taskDelete, name="task-delete")
 ]
