870a39251db75052c0e6fa80417676514c3f6b41
SalihTpc <salihtopcu@hotmail.com.tr>
2022-02-19 19:31:09 +0300

    settings changed secret_key

--- main/settings.py
+++ main/settings.py
@@ -9,10 +9,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_t99lzqh+qq582a0$5=i)+^88t6z$#b+bfm%10z*-ia-*9xt3v'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG")
 
 ALLOWED_HOSTS = []
 
